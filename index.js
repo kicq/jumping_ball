be251e651f6562a89c0db6521abc8125799ecd8f
kicq <60511189+kicq@users.noreply.github.com>
2020-11-22 20:42:34 +0300

    v1.0.1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.addEventListener('click', (evt) => {
       let color2 = Math.random().toFixed(2)*100
       let color3 = Math.random().toFixed(2)*100
       el.style.background = `rgb(${color*2}, ${color2*2}, ${color3*2})`
-    },400)
+    },410)
     
     
   }
