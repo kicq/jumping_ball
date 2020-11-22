d0171c8c7c45f4c0a86df4a5c6546326df24f371
kicq <60511189+kicq@users.noreply.github.com>
2020-11-22 20:37:32 +0300

    v1.0.0

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener('click', (evt) => {
+  const el = document.getElementById('el')
+  if (evt.target === el) {
+    let x = getRandomInt()
+    let y = getRandomInt()
+    el.style.transform = `translate(${x}%, ${y}%)`
+    setTimeout(()=>{
+      el.style.transform = `translate(${y}%, ${x}%)`
+      let color = Math.random().toFixed(2)*100
+      let color2 = Math.random().toFixed(2)*100
+      let color3 = Math.random().toFixed(2)*100
+      el.style.background = `rgb(${color*2}, ${color2*2}, ${color3*2})`
+    },400)
+    
+    
+  }
+})
+
+const getRandomInt = () => {
+  let count = Math.random().toFixed(2)
+  count *= 280
+  return count
+}
